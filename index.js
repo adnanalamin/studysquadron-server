d4265458e31e08c1ae7e65945ecdebd427d6a2e9
Al-Amin <116742755+adnanalamin@users.noreply.github.com>
2024-05-11 18:13:28 +0600

    Added assignments find with query methode

--- index.js
+++ index.js
@@ -1,20 +1,16 @@
-const express = require('express');
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const cors = require('cors');
-const jwt = require('jsonwebtoken');
-const cookieParser = require('cookie-parser');
-require('dotenv').config()
-const port = process.env.PORT || 5000
-
+const express = require("express");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const cors = require("cors");
+const jwt = require("jsonwebtoken");
+const cookieParser = require("cookie-parser");
+require("dotenv").config();
+const port = process.env.PORT || 5000;
 
 const app = express();
 
-
-app.use(cors())
-app.use(express.json())
-app.use(cookieParser())
-
-
+app.use(cors());
+app.use(express.json());
+app.use(cookieParser());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.giatfyq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
@@ -24,7 +20,7 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
@@ -32,21 +28,34 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
-    const assignmentsCollection = client.db('studysquadron').collection('assignments')
+    const assignmentsCollection = client
+      .db("studysquadron")
+      .collection("assignments");
 
-    // Save a assignments 
-    app.post('/assignment', async (req, res) => {
-        const assignmentsData = req.body
-        const result = await assignmentsCollection.insertOne(assignmentsData)
-        res.send(result)
-      })
+    // Get All Assignment
+    app.get("/all-assignment", async (req, res) => {
+      const { difficulty } = req.query;
+      let query = {};
+      if (difficulty) {
+        query = { difficultyLevel: difficulty };
+      }
+      const cursor = assignmentsCollection.find(query);
+      const result = await cursor.toArray();
+      res.send(result);
+    });
 
-
-
+    // Save a assignments
+    app.post("/assignment", async (req, res) => {
+      const assignmentsData = req.body;
+      const result = await assignmentsCollection.insertOne(assignmentsData);
+      res.send(result);
+    });
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // // Ensures that the client will close when you finish/error
     // await client.close();
@@ -54,13 +63,10 @@ async function run() {
 }
 run().catch(console.dir);
 
+app.get("/", (req, res) => {
+  res.send("server is running");
+});
 
-
-app.get('/', (req, res) => {
-    res.send("server is running")
-})
-
-
-app.listen(port, ()=>{
-    console.log('Server is Running Now')
-})
+app.listen(port, () => {
+  console.log("Server is Running Now");
+});
