f882d9622716427a992fdcd6bca9dbcacdb2d958
Al-Amin <116742755+adnanalamin@users.noreply.github.com>
2024-05-14 02:00:09 +0600

    Added Paginations Methode

--- index.js
+++ index.js
@@ -8,7 +8,16 @@ const port = process.env.PORT || 5000;
 
 const app = express();
 
-app.use(cors());
+const corsOptions = {
+  origin: [
+    'http://localhost:5173',
+    'http://localhost:5174',
+    'https://solosphere.web.app',
+  ],
+  credentials: true,
+  optionSuccessStatus: 200,
+}
+app.use(cors(corsOptions))
 app.use(express.json());
 app.use(cookieParser());
 
@@ -28,26 +37,75 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
-    const assignmentsCollection = client.db("studysquadron").collection("assignments");
-    const submitAssignmentsCollection = client.db("studysquadron").collection("SubmitAssignments");
+    const assignmentsCollection = client
+      .db("studysquadron")
+      .collection("assignments");
+    const submitAssignmentsCollection = client
+      .db("studysquadron")
+      .collection("SubmitAssignments");
+    // const submitAssignmentMarks = client
+    //   .db("studysquadron")
+    //   .collection("assignmentsMarks");
 
-    // Get All Assignment
+    // jwt 
+    app.post('/jwt', async (req, res) => {
+      const email = req.body
+      const token = jwt.sign(email, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: '365d',
+      })
+      res
+        .cookie('token', token, {
+          httpOnly: true,
+          secure: process.env.NODE_ENV === 'production',
+          sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
+        })
+        .send({ success: true })
+    })
+
+    // Clear token on logout
+    app.get('/logout', (req, res) => {
+      res
+        .clearCookie('token', {
+          httpOnly: true,
+          secure: process.env.NODE_ENV === 'production',
+          sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
+          maxAge: 0,
+        })
+        .send({ success: true })
+    })
+
+    // Get All Assignment && Paginations
     app.get("/all-assignment", async (req, res) => {
       const { difficulty } = req.query;
+      const page = parseInt(req.query.page);
+      const size = parseInt(req.query.size);
       let query = {};
       if (difficulty) {
         query = { difficultyLevel: difficulty };
       }
-      const cursor = assignmentsCollection.find(query);
-      const result = await cursor.toArray();
-      res.send(result);
+        const result = await assignmentsCollection.find(query)
+        .skip(page * size)
+        .limit(size)
+        .toArray();
+        res.send(result);
+      
     });
+    
+    
 
     // Find Assignment by ID
     app.get("/findassignment/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
       const result = await assignmentsCollection.findOne(query);
+      res.send(result);
+    });
+
+    // Find Assignment by ID
+    app.get("/giveassignmentmark/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await submitAssignmentsCollection.findOne(query);
       res.send(result);
     });
 
@@ -59,7 +117,22 @@ async function run() {
       res.send(result);
     });
 
+    app.get("/findsMyassignment/:email", async (req, res) => {
+      const email = req.params.email;
+      let query = { userEmail: email };
+      if (req.query.status === "completed") {
+        query.status = "completed";
+      }
+      const result = await submitAssignmentsCollection.find(query).toArray();
+      res.send(result);
+    });
+
     
+
+    app.get('/assignmentCount', async(req, res) => {
+      const count = await assignmentsCollection.estimatedDocumentCount();
+      res.send({count})
+    })
 
     // Save a assignments
     app.post("/assignment", async (req, res) => {
@@ -71,7 +144,38 @@ async function run() {
     // Submit a assignments
     app.post("/submit-assignment", async (req, res) => {
       const assignmentsData = req.body;
-      const result = await submitAssignmentsCollection.insertOne(assignmentsData);
+      const result = await submitAssignmentsCollection.insertOne(
+        assignmentsData
+      );
+      res.send(result);
+    });
+    // Submit a assignments Marks
+    // app.post("/assignmentMark", async (req, res) => {
+    //   const assignmentsMarksData = req.body;
+    //   const result = await submitAssignmentMarks.insertOne(
+    //     assignmentsMarksData
+    //   );
+    //   res.send(result);
+    // });
+
+    // Update Assignment status
+    app.put("/updateassignmentmark/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const options = { upsert: true };
+      const updatedData = req.body;
+      const assignmentitems = {
+        $set: {
+          status: updatedData.status,
+          number: updatedData.number,
+          assignmentFeedback: updatedData.assignmentFeedback,
+        },
+      };
+      const result = await submitAssignmentsCollection.updateOne(
+        filter,
+        assignmentitems,
+        options
+      );
       res.send(result);
     });
 
@@ -89,10 +193,13 @@ async function run() {
           thumbnailimage: updatedData.thumbnailimage,
           difficultyLevel: updatedData.difficultyLevel,
           dueDate: updatedData.dueDate,
-          
         },
       };
-      const result = await assignmentsCollection.updateOne(filter, assignmentitems, options);
+      const result = await assignmentsCollection.updateOne(
+        filter,
+        assignmentitems,
+        options
+      );
       res.send(result);
     });
 
